0520596e3019caed38caaae0acf5e9890c06d3a2
Bill Anderson <therealbill@me.com>
2015-07-06 18:45:13 +0000

    more cleanup

--- structures/structures.go
+++
@@ -1,7 +0,0 @@
-package structures
-
-type InstanceDataJSON struct {
-	Config map[string]string `json:"config"`
-	Info   string            `json:"info"`
-	Error  string            `json:"pull_error"`
-}
